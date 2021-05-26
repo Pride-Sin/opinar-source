52987824601f9ff5784c8cd54ad0508b3cb3c2e8
Nicolas <nicolasre011@gmail.com>
2021-05-26 18:41:38 -0300

    Detail view and template done. Added create view, and form template.

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PollList
+from .views import PollList, PollDetail
 
 urlpatterns = [
     path('polls/', PollList.as_view(), name='polls'),
+    path('vote/<int:pk>/', PollDetail.as_view(), name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 # Local imports
 from .models import Poll
+
+
 class PollList(ListView):
     model = Poll
     context_object_name = 'polls'
+
+
+class PollDetail(DetailView):
+    model = Poll
+    context_object_name = 'poll'
+
+class PollCreate(CreateView):
+    model = Poll
+    fields = ['question', 'organization', 'allow_anon', 'allow_comments', 'allow_results']
