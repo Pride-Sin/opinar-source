8f8b4c327d6345cc35469bbec800e5b47f46bc65
Nicolas <nicolasre011@gmail.com>
2021-06-02 20:36:57 -0300

    Deleted poll-detail, now its just poll-overview

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,11 @@
 # Django imports
 from django.urls import path
 # Local imports
-from .views import PollCreate, PollList, PollDetail, PollUpdate, PollDelete, poll_overview, vote
+from .views import PollCreate, PollList, PollUpdate, PollDelete, poll_overview, vote
 
 urlpatterns = [
     path('polls/', PollList.as_view(), name='polls'),
-    path('poll/<int:pk>/', PollDetail.as_view(), name='poll-detail'),
-    path('poll/overview/<int:poll_id>', poll_overview, name='poll-overview'),
+    path('poll/overview/<int:poll_id>/', poll_overview, name='poll-overview'),
     path('poll/new/', PollCreate.as_view(), name='poll-new'),
     path('poll/update/<int:pk>/', PollUpdate.as_view(), name='poll-update'),
     path('poll/delete/<int:pk>/', PollDelete.as_view(), name='poll-delete'),
